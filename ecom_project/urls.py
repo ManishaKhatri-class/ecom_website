b5469b32c230cb77591f92f500286b97d8206cf7
ManishaKhatri-class <codeblogger17@gmail.com>
2025-12-27 10:54:25 +0530

    home path added

--- ecom_project/urls.py
+++ ecom_project/urls.py
@@ -4,12 +4,14 @@ from django.urls import path,include
 from admin_panel import views as admin_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
   
 
 
 urlpatterns = [
  path('home/',admin_views.home,name="home"),
+ path('',RedirectView.as_view(url='/home/'),permament=True)
     path('admin-panel/',admin_views.admin_login,name="admin-login"),
       path('admin-panel/logout',admin_views.admin_logout,name="admin-logout"),
     path('admin-dashboard/',admin_views.admin_dashboard,name="admin-dashboard"),
